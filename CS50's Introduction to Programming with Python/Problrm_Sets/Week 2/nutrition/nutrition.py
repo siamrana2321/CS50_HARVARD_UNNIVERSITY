bb55f74c4f4e38eae6a0748b4e1646e3cb3797d9
Mohammad Siam Ahmed Rana <msar572321@gmail.com>
2025-04-03 21:21:10 +0600

    Add files via upload

---
+++ CS50's Introduction to Programming with Python/Problrm_Sets/Week 2/nutrition/nutrition.py
@@ -0,0 +1,42 @@
+x = input("Item: ")
+
+if x.lower() == "apple" :
+    print("Calories: ",130)
+elif x.lower() == "avocado" :
+    print("Calories: ",50)
+elif x.lower() == "banana" :
+    print("Calories: ",110)
+elif x.lower() == "cantaloupe" :
+    print("Calories: ",50)
+elif x.lower() == "grapefruit" :
+    print("Calories: ",60)
+elif x.lower() == "grapes" :
+    print("Calories: ",90)
+elif x.lower() == "honeydew melon" :
+    print("Calories: ",50)
+elif x.lower() == "kiwifruit" :
+    print("Calories: ",90)
+elif x.lower() == "lemon" :
+    print("Calories: ",15)
+elif x.lower() == "lime" :
+    print("Calories: ",20)
+elif x.lower() == "nectarine" :
+    print("Calories: ",60)
+elif x.lower() == "orange" :
+    print("Calories: ",80)
+elif x.lower() == "peach" :
+    print("Calories: ",60)
+elif x.lower() == "pear" :
+    print("Calories: ",100)
+elif x.lower() == "pineapple" :
+    print("Calories: ",50)
+elif x.lower() == "plums" :
+    print("Calories: ",70)
+elif x.lower() == "strawberries" :
+    print("Calories: ",50)
+elif x.lower() == "sweet cherries" :
+    print("Calories: ",100)
+elif x.lower() == "tangerine" :
+    print("Calories: ",50)
+elif x.lower() == "watermelon" :
+    print("Calories: ",80)
